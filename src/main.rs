df1924839ef5230035f17e1bc268a08bd4527f49
Agnese Kapnere <akapne01@gmail.com>
2022-10-15 16:56:31 +0100

    Explore tuples in rust

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod print;
 mod vars;
 mod types;
 mod strings;
+mod tuples;
 
 fn main() {
     print::run();
     vars::run();
     types::run();
     strings::run();
+    tuples::run();
 }
---
+++ src/tuples.rs
@@ -0,0 +1,10 @@
+/*
+Tuples group together values of different types
+Max 12 elements.
+*/
+
+pub fn run() {
+    let person: (&str, &str, i8) = ("Alice", "Wonderland", 37);
+
+    println!("{} is from {} and is {}", person.0, person.1, person.2);
+}
