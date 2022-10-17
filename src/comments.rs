219b6bb8301b862c5a36937c0804cb92d8bac3f8
Agnese Kapnere <akapne01@gmail.com>
2022-10-17 17:11:53 +0100

    Comments in rust

---
+++ src/comments.rs
@@ -0,0 +1,10 @@
+/*
+Documentation: https://doc.rust-lang.org/book/ch03-04-comments.html
+*/
+
+// This is a comment in rust
+// Multiline. 
+
+// Place it on a separate line above the code it's annotating. 
+
+// Rust also has documentation type comments with /// which will be explored separately. 
